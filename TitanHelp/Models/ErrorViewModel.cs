2fa402d03938f0db4081fb9b6298b89e78a494c3
Shane Madden <Madrox251@aol.com>
2023-02-09 23:17:08 -0500

    Add project files.

---
+++ TitanHelp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TitanHelp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
