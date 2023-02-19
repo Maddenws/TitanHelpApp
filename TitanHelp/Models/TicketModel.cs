bfcbd94fd43f136226a4fe999979a9734c70142c
Shane Madden <Madrox251@aol.com>
2023-02-19 01:48:34 -0500

    Added Ticket Model

---
+++ TitanHelp/Models/TicketModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TitanHelp.Models
+{
+    public class TicketModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime DateCreated{ get; set; }
+        public string? Description { get; set; }
+    }
+}
