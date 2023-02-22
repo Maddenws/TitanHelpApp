e34d9a3c05871a94408d133fd0c576e66dc11fd3
Xenon <99380219+xenonwolfy@users.noreply.github.com>
2023-02-21 20:50:59 -0500

    DB-Migration-Init
    Init migration of database
    DB only contains creation of table for Tickets in database

---
+++ TitanHelp/Migrations/20230222011315_TitanData-Init-Migration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TitanHelp.Migrations
+{
+    public partial class TitanDataInitMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TicketModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TicketModel", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TicketModel");
+        }
+    }
+}
